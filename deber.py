487c7480bbb342081e313f569455263361c54348
Edison <molinaedison5@gmail.com>
2020-11-25 14:57:55 -0500

    tabla

--- deber.py
+++ deber.py
@@ -1,4 +1,48 @@
 #tabla
+a = int(input("Ingrese numero de tablas: "))
 
-a = int(input("Ingrese numero de tablas"))
-b = int(input("Ingrese "))
+b = int(input("Ingrese rango: "))
+
+cont1 = 0
+
+for i in range(2, a+1):# numero de tablas
+
+    print("     Tabla --> ",i)
+
+    cont=0 #reiniciar contador
+
+    contResta = 0
+
+    for j in range(b):# rango de las tablas
+
+        j=j+1
+
+        sum = j+i
+
+        print(j,"+",i,"=",sum)#impresion tabla
+
+        if j%2==1:#numeros impares
+
+            cont+=sum #sumatoria de cada tabla
+
+            cont1+=sum #sumatoria total
+    
+        contResta+=sum #sumatoria de ultima tabla
+
+    print("Valor tabla -> ",i,": ",cont) 
+    
+print("Valor de la ultima tabla --> ", contResta)
+
+print("Valor de las tablas: ->",cont1)
+
+print("RESPUESTA FINAL ES: ", cont1 - contResta)
+
+
+    
+            
+            
+
+
+
+
+
